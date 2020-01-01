b5538e336b37def79535c0eb8bfc2af89be87eff
Barney Loosemore <barney@barney.co.uk>
2020-01-01 23:59:29 +0000

    Init Commit

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,5 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+import { registerLinkResolver } from "gatsby-source-prismic-graphql"
 
-// You can delete this file if you're not using it
+import { linkResolver } from "./src/utils/linkResolver"
+
+registerLinkResolver(linkResolver)
