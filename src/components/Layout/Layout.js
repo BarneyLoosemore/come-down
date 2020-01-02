2c61d731eb35f6f1ef2e00bfebaeb51f66531cd1
Barney Loosemore <barney@barney.co.uk>
2020-01-02 00:01:36 +0000

    3rd commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,55 @@
+import React from "react"
+import { createGlobalStyle } from "styled-components"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: 'Oswald', sans-serif;
+    background-size: 100%;
+    background-image: url(${({ backgroundImage }) =>
+      backgroundImage
+        ? backgroundImage
+        : "https://images.prismic.io/comedown/bf526cb0-331d-4fb1-9068-5ab2563e04d8_ComedownBackground.jpg?auto=compress,format"});
+  }
+  h1 {
+    font-size: 2em;
+    margin: 0.67em 0;
+  }
+  a:focus & {
+    outline: 5px solid orange;
+  }
+`
+
+export const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <GlobalStyles />
+      <link
+        href="https://fonts.googleapis.com/css?family=Oswald:200,400,700&display=swap"
+        rel="stylesheet"
+      ></link>
+      {typeof window !== "undefined" ? "window" : "no window"}
+      <main>{children}</main>
+      <div style={{ marginBottom: "50px" }} />
+      <footer style={{ margin: "8px", color: "white" }}>
+        {data.site && data.site.siteMetadata.title}
+      </footer>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,42 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { Layout } from "../components/Layout"
+import { SEO } from "../components/SEO"
+// import { ContentBox } from "../components/ContentBox"
+
+const StyledLink = styled(Link)`
+  :hover {
+    opacity: 0.7;
+  }
+  font-size: 48px;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin: 32px 0;
+  height: 100%;
+`
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 150px;
+  @media(max-width: 640px){
+    margin 75px;
+  };
+`
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <ContentBox>
+      <Container>
+        {/* <StyledLink to="/about">About</StyledLink>
+        <StyledLink to="/1">Comic</StyledLink> */}
+        index
+      </Container>
+    </ContentBox>
   </Layout>
 )
 
