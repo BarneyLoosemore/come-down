0063edad7e89e0c7345423f66506c02fb171c2a5
Barney Loosemore <barney@barney.co.uk>
2021-03-08 22:17:01 +0000

    Alternate solution to initial redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
-// for requests to '/', we redirect to the first page of the latest chapter
-export default () => null
+import { useRouter } from "next/router"
+
+export default () => {
+  const router = useRouter()
+
+  // for requests to '/', we redirect to the first page of the latest chapter
+  router.replace("/pages/49")
+  return null
+}
 
 export const getStaticProps = () => {
   return { props: {} }
