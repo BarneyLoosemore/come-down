4513cadd1c8f5982f5b1cfc6165c08c01e795761
Barney Loosemore <barney@barney.co.uk>
2021-03-09 11:50:20 +0000

    Fix '/' redirect, again..

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
+import { useRouter } from "next/router"
+
 import { ContentBox } from "@components/ContentBox"
-import { redirect } from "@utils/redirect"
 
-export default () => <ContentBox />
+export default () => {
+  const router = useRouter()
 
-export const getServerSideProps = ({ res }) => {
   // HACK: for requests to '/', we redirect to the first page of the latest chapter
   // ideally would use `redirects()` in next.config.js, but this wasn't working on prod
-  redirect({ res, target: "/pages/49" })
+  if (typeof window !== "undefined") {
+    router.replace("/pages/49")
+  }
+  return <ContentBox />
+}
+export const getStaticProps = ctx => {
   return { props: {} }
 }
